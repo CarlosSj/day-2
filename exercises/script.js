243dff6dd29f9c6ecd80c53487fdaf56fc431340
Carlos Sanchez <carlos@Leylas-MacBook-Pro.local>
2018-07-11 17:07:06 -0400

    committing day-2

--- exercises/script.js
+++ exercises/script.js
@@ -1,58 +1,71 @@
 document.querySelector('.puzzle-0').addEventListener('click', function() {
-	  window.alert('Hello, World!');
+	  alert('Hello, World!');
 	});
 
 	document.querySelector('.puzzle-1').addEventListener('click', function() {
-		
+		alert('Bonjour, Monde!')
 	});
 
 	document.querySelector('.puzzle-2').addEventListener('click', function() {
-
+		document.querySelector('h1').textContent = "ADs ADS Ads";
 	});
 
 	document.querySelector('.puzzle-3').addEventListener('click', function() {
+		// document.querySelector('img').src="https://media.giphy.com/media/WXB88TeARFVvi/giphy.gif";
 
 	});
 
 	document.querySelector('.puzzle-4').addEventListener('click', function() {
-
+		document.querySelector('img').style.visibility = "hidden";
 	});
 
 	document.querySelector('.puzzle-5').addEventListener('click', function() {
-
+		document.querySelector('img').style.visibility = "visible";
 	});
 
 	document.querySelector('.puzzle-6-yellow').addEventListener('click', function() {
-		
+	document.querySelector('body').style.background = "yellow";
 	});
 
 	document.querySelector('.puzzle-6-pink').addEventListener('click', function() {
-		
+	document.querySelector('body').style.background = "pink";
 	});
-
 	document.querySelector('.puzzle-6-purple').addEventListener('click', function() {
-		
+	document.querySelector('body').style.background = "purple";	
 	});
 
 	document.querySelector('.puzzle-7').addEventListener('click', function() {
-		
+	document.querySelector('html').style.color = "red";
+	document.querySelector('body').style.fontFamily= "Comic Sans MS";
 	});
 
 	document.querySelector('.puzzle-8').addEventListener('click', function() {
-
+	document.querySelector('a').href= "https://www.youtube.com/";
+	document.querySelector('a').textContent= "go to youtubes";
 	});
 
 	// Challenge puzzles
 
-	document.querySelector('.puzzle-9')
+	document.querySelector('.puzzle-9').addEventListener('click', function() {
+	document.querySelector('body').style.fontFamily= "Monaco";		
+	});
 
-	document.querySelector('.puzzle-10')
+	document.querySelector('.puzzle-10').addEventListener('click', function() {
+	document.querySelector("song.mp3").play('')
+	});
 
-	document.querySelector('.puzzle-11')
+	document.querySelector('.puzzle-11').addEventListener('click', function() {
+		console.log('hi');
+		document.querySelector('.gakko').innerHTML = "<img src='https://www.gakkoproject.com/assets/events/default-9738f5c9c993c457180655d4e3ff69be341a0c3436f12f0525e1b936a359d426.png'>";
+	});
 
-	document.querySelector('.puzzle-12')
+	document.querySelector('.puzzle-12').addEventListener('click', function() {
+	document.querySelector('body').style.color = "blue";
+	});
 
-	document.querySelector('.puzzle-13')
+	document.querySelector('.puzzle-13').addEventListener('click', function() {
+		document.querySelector(".marquee").innerHTML ="<marquee> I wish I was a marquee </marquee>";
+	});
 
 	document.querySelector('.puzzle-14')
 
